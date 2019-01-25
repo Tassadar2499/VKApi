393f9436ae35043814396b1c48eb32c6dff9f1c2
Tassadar2499 <34176883+Tassadar2499@users.noreply.github.com>
2019-01-25 06:11:31 +0500

    Добавил обработку исключений, отрефакторил

--- VKApi/WeatherAP.cs
+++ VKApi/WeatherAP.cs
@@ -14,26 +14,41 @@ namespace VKApi
         public static string GetWeather(string city)
         {
             var result = "";
-            //var city = "London";
             var key = "&appid=03133e2dfe331abba3e6b5912b3fa4a8";
             string weburl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + key;
-            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(weburl);
-            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-            string response;
-            using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
-                response = streamReader.ReadToEnd();
-            result = ConvertCityWeather(response, city);
+            try
+            {
+                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(weburl);
+                HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                string response;
+                using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+                    response = streamReader.ReadToEnd();
+                result = ConvertCityWeather(response, city);
+            }
+            catch
+            {
+                result = "Неверный формат строки";
+                Console.WriteLine(result);
+            }
 
             return result;
         }
 
         private static string ConvertCityWeather(string response, string cityName)
         {
+            Func<string[], string, string> findStrByQuery = 
+                (string[] strArray, string query) =>
+            {
+                return strArray.Where(l => l.Contains(query)).ToArray()[0];
+            };
+         
             var cityWeather = new CityWeather(cityName);
-            var strArr = response.Split(',');
-            var strTemperature = strArr[7];
-            var strCloud = strArr[4];
-            var strWind = strArr[13];
+            var strArr = response.Split(',');            
+            strArr = strArr.Select(l => l.Replace('"', ' '))
+                           .ToArray();
+            var strTemperature = findStrByQuery(strArr, "temp");
+            var strCloud = findStrByQuery(strArr, "description");
+            var strWind = findStrByQuery(strArr, "wind");
             strTemperature = strTemperature.Substring(strTemperature.LastIndexOf(':') + 1);
             strCloud = strCloud.Substring(strCloud.LastIndexOf(':') + 2, strCloud.Length - (strCloud.LastIndexOf(':') + 3));
             strWind = strWind.Substring(strWind.LastIndexOf(':') + 1);
